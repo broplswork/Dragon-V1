8bbb3348817d8f8d6667a7b5c197373a79feb976
broplswork <143388875+broplswork@users.noreply.github.com>
2023-11-12 02:27:54 +0000

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // app.js
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Sample game data
 const games = [
-    { id: 1, name: 'Game 1', description: 'Description of Game 1' },
-    { id: 2, name: 'Game 2', description: 'Description of Game 2' },
+    { id: 1, name: 'Guessing Game', description: 'Try to guess the correct number between 1 and 10' },
     // Add more games as needed
 ];
 
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Home page
 app.get('/', (req, res) => {
@@ -30,6 +31,25 @@ app.get('/games/:id', (req, res) => {
 
     if (game) {
         res.render('game', { game });
+    } else {
+        res.status(404).send('Game not found');
+    }
+});
+
+// Guessing form submission
+app.post('/games/:id/guess', (req, res) => {
+    const gameId = parseInt(req.params.id);
+    const game = games.find(g => g.id === gameId);
+
+    if (game) {
+        const secretNumber = Math.floor(Math.random() * 10) + 1;
+        const userGuess = parseInt(req.body.userGuess);
+
+        if (userGuess === secretNumber) {
+            res.send(`Congratulations! You guessed the correct number (${secretNumber}) in ${req.body.userGuess} tries.`);
+        } else {
+            res.send(`Sorry, the correct number was ${secretNumber}. Try again!`);
+        }
     } else {
         res.status(404).send('Game not found');
     }
