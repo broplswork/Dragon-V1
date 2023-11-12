cbd9b67b0e488251f8912980b20a72b2591700c9
broplswork <143388875+broplswork@users.noreply.github.com>
2023-11-12 05:02:23 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,36 @@ const proxy = require('express-http-proxy');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Sample game data
+// Configuration
 const games = [
     { id: 1, name: 'Guessing Game', description: 'Try to guess the correct number between 1 and 10' },
     { id: 2, name: 'Roblox', description: 'Play Roblox', link: '/games/roblox/play' },
     // Add more games as needed
 ];
 
-// Set the views folder and use EJS as the template engine
+const proxyUrlMap = {
+    google: 'https://www.google.com/search?q=',
+    bing: 'https://www.bing.com/search?q=',
+    // Add more search engines and their proxy URLs as needed
+};
+
+// Middleware for checking ad-blockers
+const adBlockerMiddleware = (req, res, next) => {
+    const isAdBlocked = req.query.adblock === 'true';
+    res.locals.isAdBlocked = isAdBlocked;
+    next();
+};
+
+// Set up views and static files
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(adBlockerMiddleware);
 
 // Home page
 app.get('/', (req, res) => {
-    const isAdBlocked = req.query.adblock === 'true';
-    res.render('index', { games, isAdBlocked });
+    res.render('index', { games });
 });
 
 // Individual game page
@@ -39,34 +50,22 @@ app.get('/games/:id', (req, res) => {
     }
 });
 
-// Proxy route for Roblox
-app.use('/games/roblox/play', proxy('https://now.gg/es/apps/roblox-corporation/5349/roblox.html'));
-
-// Search route
+// Proxy route for search
 app.get('/search', (req, res) => {
     const query = req.query.q;
     const searchEngine = req.query.engine;
 
-    if (!query || !searchEngine) {
+    if (!query || !searchEngine || !proxyUrlMap[searchEngine]) {
         res.render('search', { results: null });
         return;
     }
 
-    // Define the proxy URL for each search engine
-    const proxyUrlMap = {
-        google: `https://www.google.com/search?q=${encodeURIComponent(query)}`,
-        bing: `https://www.bing.com/search?q=${encodeURIComponent(query)}`,
-        // Add more search engines and their proxy URLs as needed
-    };
-
-    // Proxy the search request
-    const proxyUrl = proxyUrlMap[searchEngine];
-    if (proxyUrl) {
-        res.render('search', { results: proxyUrl });
-    } else {
-        res.status(400).send('Invalid search engine');
-    }
+    const proxyUrl = proxyUrlMap[searchEngine] + encodeURIComponent(query);
+    res.render('search', { results: proxyUrl });
 });
+
+// Proxy route for Roblox
+app.use('/games/roblox/play', proxy('https://now.gg/es/apps/roblox-corporation/5349/roblox.html'));
 
 // Guessing form submission
 app.post('/games/:id/guess', (req, res) => {
@@ -87,9 +86,18 @@ app.post('/games/:id/guess', (req, res) => {
     }
 });
 
+// Error handling for 404
+app.use((req, res, next) => {
+    res.status(404).send('Page not found');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 // Start the server
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
